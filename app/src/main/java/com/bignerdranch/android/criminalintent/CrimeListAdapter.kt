970a84e8e6fb700fb47643d29992512a11418c79
Sarah Khan <sarkhan@bu.edu>
2024-02-28 20:35:38 -0500

    added row for serious crimes

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
+import com.bignerdranch.android.criminalintent.databinding.ListItemSeriousCrimeBinding
+import java.lang.IllegalArgumentException
 
 class CrimeHolder(
     private val binding: ListItemCrimeBinding
@@ -23,22 +25,56 @@ class CrimeHolder(
     }
 }
 
-class CrimeListAdapter(
-    private val crimes: List<Crime>
-) : RecyclerView.Adapter<CrimeHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): CrimeHolder {
+class SeriousCrimeHolder(
+    private val binding: ListItemSeriousCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "serious ${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
+
+class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        private const val normal_view = 0
+        private const val serious_view = 1
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
-        return CrimeHolder(binding)
+        return when(viewType) {
+            normal_view -> {
+                val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+                CrimeHolder(binding)
+            }
+            serious_view -> {
+                val binding = ListItemSeriousCrimeBinding.inflate(inflater, parent, false)
+                SeriousCrimeHolder(binding)
+            }
+            else -> throw IllegalArgumentException("no view found")
+        }
+
+
     }
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val crime = crimes[position]
-        holder.bind(crime)
+        when (holder) {
+            is CrimeHolder -> holder.bind(crime)
+            is SeriousCrimeHolder -> holder.bind(crime)
+        }
     }
 
     override fun getItemCount() = crimes.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if (crimes[position].requiresPolice) serious_view else normal_view
+    }
 }
